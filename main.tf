b50109e4c870826646e614f504074b5c095ec892
Vincent Partington <vpartington@xebialabs.com>
2018-04-14 15:21:01 +0200

    Added autoscaling group

--- main.tf
+++ main.tf
@@ -7,19 +7,34 @@ variable "webserver_port" {
   default = 8282
 }
 
-output "webserver_ip" {
-  value = "${aws_instance.webserver.public_ip}"
+#output "webserver_ip" {
+#  value = "${aws_instance.webserver.public_ip}"
+#}
+
+resource "aws_security_group" "webserver_security_group" {
+  name = "Created by vpartington with terraform"
+
+  ingress {
+    from_port = "${var.webserver_port}"
+    to_port = "${var.webserver_port}"
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_instance" "webserver" {
-#  ami = "ami-1b791862" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in eu-west-1, which has no default VPC)
-#  ami = "ami-941e04f0" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in eu-west-2, which does not support autoscaling groups)
-#  ami = "ami-40d28157" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in us-east-1, which has no default VPC)
-  ami = "ami-965e6bf3" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in us-east-2)
-#  ami = "ami-79873901" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in us-west-2, which has no default VPC)
+resource "aws_launch_configuration" "webserver" {
+#  image_id = "ami-1b791862" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in eu-west-1, which has no default VPC)
+#  image_id = "ami-941e04f0" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in eu-west-2, which does not support autoscaling groups)
+#  image_id = "ami-40d28157" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in us-east-1, which has no default VPC)
+  image_id = "ami-965e6bf3" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in us-east-2)
+#  image_id = "ami-79873901" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in us-west-2, which has no default VPC)
 
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ "${aws_security_group.webserver_security_group.id}" ]
+  security_groups = [ "${aws_security_group.webserver_security_group.id}" ]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -27,17 +42,20 @@ resource "aws_instance" "webserver" {
               nohup busybox httpd -f -p ${var.webserver_port} &
               EOF
 
-  tags {
-    Name = "Created by vpartington with terraform"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-resource "aws_security_group" "webserver_security_group" {
-  name = "terraform-example-instance"
-  ingress {
-    from_port = "${var.webserver_port}"
-    to_port = "${var.webserver_port}"
-    protocol = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
+resource "aws_autoscaling_group" "webserver_group" {
+  launch_configuration = "${aws_launch_configuration.webserver.id}"
+  min_size = 2
+  max_size = 10
+
+  availability_zones = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  tag {
+    key = "Name"
+    value = "Created by vpartington with terraform"
+    propagate_at_launch = true
   }
 }
