f225a0b7aebf1c15421879daad3cd38c4e81f31a
Vincent Partington <vpartington@xebialabs.com>
2018-04-14 15:17:26 +0200

    Added ami IDs for many regions

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-east-2"
 }
 
 variable "webserver_port" {
@@ -12,7 +12,12 @@ output "webserver_ip" {
 }
 
 resource "aws_instance" "webserver" {
-  ami = "ami-941e04f0"
+#  ami = "ami-1b791862" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in eu-west-1, which has no default VPC)
+#  ami = "ami-941e04f0" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in eu-west-2, which does not support autoscaling groups)
+#  ami = "ami-40d28157" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in us-east-1, which has no default VPC)
+  ami = "ami-965e6bf3" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in us-east-2)
+#  ami = "ami-79873901" # (ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126 in us-west-2, which has no default VPC)
+
   instance_type = "t2.micro"
   vpc_security_group_ids = [ "${aws_security_group.webserver_security_group.id}" ]
 
